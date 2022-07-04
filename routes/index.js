0fb0c04fb9ae106872283399a9ea783813492ce5
David <davidmdz20@outlook.com>
2022-07-03 20:10:24 -0600

    Tarea de recuperacion de contraseña

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', alumno:'David Mendoza 0601-1998-00243'});
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
